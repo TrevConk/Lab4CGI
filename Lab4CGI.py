e8da7a571412de162cdda9badd2731ebbb0819ef
Trevor Conklin <91498857+TrevConk@users.noreply.github.com>
2021-10-20 00:36:20 +0000

    removed form stuff

--- Lab4CGI.py
+++ Lab4CGI.py
@@ -7,14 +7,12 @@ LEDNum = data.getvalue('LED')
 BrightnessNum = data.getvalue('Brightness')
 
 dumpData = {'LED':LEDNum, 'Brightness':BrightnessNum}
-with open('Lab4DataDup.txt','w') as f:
+with open('Lab4DataDump.txt','w') as f:
   json.dump(dumpData,f)
 
 print('Content-type: text/html\n\n')
 print('<html>')
-print('<form action="/cgi-bin/Lab4CGI.py" method="Post">')
 print('selection = ' + LEDNum)
 print('<input type="range" name="brightness" min="0" max="100" value="%s"><br>' % BrightnessNum)
-print('</form>')
 print('<Brightness is %s' %BrightnessNum)
 print('</html>')
