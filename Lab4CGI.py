a1d4f84dd35983ce2f63a3df77d018363d0fddf5
Trevor Conklin <91498857+TrevConk@users.noreply.github.com>
2021-10-20 05:06:56 +0000

    large change

--- Lab4CGI.py
+++ Lab4CGI.py
@@ -2,40 +2,26 @@
 import cgi
 import cgitb
 import json
-cgitb.enable()
 
+cgitb.enable()
 data = cgi.FieldStorage()
 
-LEDNum = data.getvalue('LED')
-BrightnessNum = data.getvalue('Brightness')
+LED = data.getvalue('LEDpin')
+Brightness = data.getvalue('BrightVal')
+dataDump = {'LEDpin':LED,'BrightVal':Brightness}
+with open('Lab4DataDump.txt', 'w') as f:
+  json.dump(dataDump,f)
 
-dumpData = {'LED':LEDNum, 'Brightness':BrightnessNum}
-with open('Lab4DataDump.txt','w') as f:
-  json.dump(dumpData,f)
-
-#Setting up html page
 print('Content-type: text/html\n\n')
-print('<html>')
-print('<head>')
-print('<title>Lab4</title>')# 
-print('</head>')
-print('<body>')
-print('<div style="width:600px;background:#AAAAFF;border:1px;textalign:center">')
-print('<br>')
-print('<font size="3" color="black" face="helvetica">')
-
-#adding in changing values
-print('<form action="/cgi-bin/Lab4CGI.py" method="POST">')
-print('<input type="radio" name="LED" value="1" checked> LED 1 <br>')
-print('<input type="radio" name="LED" value="2"> LED 2 <br>')
-print('<input type="radio" name="LED" value="3"> LED 3 <br>')
-print('<input type="range" name="brightness" min="0" max="100" value="%s"><br>' % BrightnessNum)
-print('<input type="submit" value="Submit">')
-
-
-print('<b>',BrightnessNum,'</b')
-
-
-print ('<br>')
-print ('</body>')
-print ('</html>')
+print('''
+<html>
+<form action="/cgi-bin/4cgi.py" method="POST">
+  <input type="radio" name="LEDPin" value="1"> LED 1 <br>
+  <input type="radio" name="LEDPin" value="2"> LED 2 <br>
+  <input type="radio" name="LEDPin" value="3"> LED 3 <br>
+  <input type="submit" value="Submit">
+''')
+print('<input type="range" name="BrightBal" min="0" max="100" value="%s"><br>' % Brightness)
+print('</form>')
+print('<b>Brightness = %s</b>' % Brightness)
+print('</html>')
