4ee4d5ef694e39b73c4dd091b2221a1085b48de6
Trevor Conklin <91498857+TrevConk@users.noreply.github.com>
2021-10-20 01:56:21 +0000

    updated to match html

--- Lab4CGI.py
+++ Lab4CGI.py
@@ -10,9 +10,28 @@ dumpData = {'LED':LEDNum, 'Brightness':BrightnessNum}
 with open('Lab4DataDump.txt','w') as f:
   json.dump(dumpData,f)
 
+#Setting up html page
 print('Content-type: text/html\n\n')
 print('<html>')
+print('<head>')
+print('<title>Lab4</title>')
+print('<meta http-equiv="refresh" content="2">') # 
+print('</head>')
+print('<body>')
+print('<div style="width:600px;background:#AAAAFF;border:1px;textalign:center">')
+print('<br>')
+print('<font size="3" color="black" face="helvetica">')
 print('selection = ' + LEDNum)
+
+#adding in changing values
+print('<form action="/cgi-bin/Lab4CGI.py" method="POST" target="_self">')
+print('<input type="radio" name="LED" value="1"> LED 1 <br>')
+print('<input type="radio" name="LED" value="2"> LED 2 <br>')
+print('<input type="radio" name="LED" value="3"> LED 3 <br>')
 print('<input type="range" name="brightness" min="0" max="100" value="%s"><br>' % BrightnessNum)
 print('<Brightness is %s' %BrightnessNum)
-print('</html>')
+
+
+print ('<br>')
+print ('</body>')
+print ('</html>')
